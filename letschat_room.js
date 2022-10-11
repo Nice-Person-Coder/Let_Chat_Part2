c71c5a28ccb0dd79d525bf157514e0fd5130fa2a
Nice-Person-Coder <110208689+Nice-Person-Coder@users.noreply.github.com>
2022-10-10 20:11:34 -0500

    Add files via upload

---
+++ letschat_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDW6DR8m1dDF8f9uuzSP_0izXe2vxDLzO0",
+    authDomain: "kwitter-422e4.firebaseapp.com",
+    databaseURL: "https://kwitter-422e4-default-rtdb.firebaseio.com",
+    projectId: "kwitter-422e4",
+    storageBucket: "kwitter-422e4.appspot.com",
+    messagingSenderId: "572163698594",
+    appId: "1:572163698594:web:b18ea1098c967b40020cfb"
+  };
+  firebase.initializeApp(firebaseConfig);
+ 
